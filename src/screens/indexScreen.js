e2cc4cf8a7a9ac9353c7348a6876a46c8a122441
Mauro Castro <49006241+MauroCastro1705@users.noreply.github.com>
2020-08-04 20:57:23 -0300

    comienzo del proyecto

---
+++ src/screens/indexScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text> pijar larga</Text>
+    </View>
+  );
+};
+
+const estilos = StyleSheet.create({});
+
+export default IndexScreen;
